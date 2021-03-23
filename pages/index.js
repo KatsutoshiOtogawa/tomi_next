69b62cde5044e07e4ad7268f7d79a19065c12dbf
KatsutoshiOtogawa <13373011+KatsutoshiOtogawa@users.noreply.github.com>
2021-03-24 00:13:55 +0900

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Link from "next/link";
+import Typography from "@material-ui/core/Typography";
+
+export default function IndexPage() {
+  return (
+    <div>
+      <Typography variant="h4">
+        ここは十味ちゃんの非公式ファンサイトです。
+      </Typography>
+      <p>
+        十味ちゃんのTwitter,インスタグラムなどなどいろいろなSNSをこれで追う事ができます。😆
+        twitter,インスタグラムなどの面倒な登録をせずに!
+      </p>
+    </div>
+  );
+}
